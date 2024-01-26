1236b71e6fb67817aafd8b3591a1d54d274e72a5
Hassaan Ahmed <106430586+HassaanAhmed60211@users.noreply.github.com>
2024-01-26 22:07:13 +0500

    Updates Jan 26
    make a static website for pizza for practice using html and css

---
+++ landing_page.js
@@ -0,0 +1,14 @@
+function onPageContact(){
+    window.location.href = 'contact.html';
+}
+
+function onPageHome(){
+    window.location.href = 'landing_page.html';
+}
+function onPageOrder(){
+    window.location.href = 'order.html';
+}
+
+function onPageMenu(){
+    window.location.href = 'menu.html';
+}
